1c3444c128224d3c39e4acba590d3340f0484a69
AldiyarAz <104771997+AldiyarAz@users.noreply.github.com>
2024-05-25 00:59:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+function scrollToElement(elementSelector, instance = 0) { 
+    // Select all elements that match the given selector 
+    const elements = document.querySelectorAll(elementSelector); 
+    // Check if there are elements matching the selector and if the requested instance exists 
+    if (elements.length > instance) { 
+        // Scroll to the specified instance of the element 
+        elements[instance].scrollIntoView({ behavior: 'smooth' }); 
+    } 
+} 
+ 
+const link1 = document.getElementById("link1"); 
+const link2 = document.getElementById("link2"); 
+const link3 = document.getElementById("link3");
+const link4 = document.getElementById("link4");  
+ 
+link1.addEventListener('click', () => { 
+    scrollToElement('.header'); 
+}); 
+ 
+link2.addEventListener('click', () => { 
+    // Scroll to the second element with "header" class 
+    scrollToElement('.header', 1); 
+}); 
+ 
+link3.addEventListener('click', () => { 
+    scrollToElement('.column'); 
+});
+
+link4.addEventListener('click', () => { 
+  // Scroll to the second element with "header" class 
+  scrollToElement('.header', 2); 
+}); 
+var slideIndex = 0;
+
+function showSlides() {
+  var slides = document.getElementsByClassName("mySlides");
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000); 
+}
+
+showSlides(); 
+
+
+
+
+
+
+
