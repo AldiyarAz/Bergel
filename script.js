ad4f28a71431612c9be4279bbc92ce497ab17324
AldiyarAz <104771997+AldiyarAz@users.noreply.github.com>
2024-05-26 17:07:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -31,10 +31,11 @@ link4.addEventListener('click', () => {
   scrollToElement('.header', 2); 
 }); 
 var slideIndex = 0;
-
+let slideindex = 0;
+showslides();
 function showSlides() {
-  var slides = document.getElementsByClassName("mySlides");
-  for (var i = 0; i < slides.length; i++) {
+  let slides = document.getElementsByClassName("mySlides");
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
